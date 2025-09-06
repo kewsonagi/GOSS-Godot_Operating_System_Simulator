1418fa1aa132546f00413176b165a7985354e145
kewsonagi <kewsonagi@gmail.com>
2025-09-06 11:25:42 -0400

    remove unused files

--- addons/multi_spliter_container/helpers.gd
+++
@@ -1,6 +0,0 @@
-extends Object
-
-class_name Helpers
-
-static func get_editor_interface():
-	return Engine.get_singleton("EditorInterface")
--- addons/rakugo-nodes/examples/button_container_example.gd
+++
@@ -1,9 +0,0 @@
-@tool
-extends ButtonContainer
-
-@onready var label: Label = $Label
-
-
-func _on_toggled(value: bool) -> void:
-	if value: label.text = "Pressed"
-	else: label.text = "UnPressed"
